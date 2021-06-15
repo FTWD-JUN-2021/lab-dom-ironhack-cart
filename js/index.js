c4f87d32508b1dc71b8a06a0db4530f75a839d99
Mahim Pritom <mahimnyc@yahoo.com>
2021-06-15 17:18:12 -0400

    bonus done

--- js/index.js
+++ js/index.js
@@ -5,16 +5,30 @@ function updateSubtotal(product) {
 
   let subtotal = price * quantity;
   product.querySelector('.subtotal span').innerHTML = subtotal;
-  return subtotal
+  return subtotal;
 }
 
 document.querySelector('#calculate').onclick = function (event) {
   let rows = document.querySelectorAll('.product');
 
-  let sum = 0;  
+  let sum = 0;
   for (let row of rows) {
     sum += updateSubtotal(row);
     console.log(sum);
   }
   document.querySelector('#total-value span').innerText = sum;
+};
+
+let allButton = document.querySelectorAll('.btn-remove');
+for (let button of allButton) {
+  button.onclick = function (e) {
+    console.log(button);
+
+    this.parentNode.parentNode.remove(); //e.target,button
+  };
 }
+
+// document.querySelectorAll('.btn-remove').onclick = function (event) {
+//   console.log('Tiger');
+//   document.querySelector('.product').remove();
+// };
