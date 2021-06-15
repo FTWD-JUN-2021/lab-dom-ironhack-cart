ef2e29390453db9517bb74f1abcf11ba1af2c281
leannewilson <83836254+leannewilson@users.noreply.github.com>
2021-06-15 13:03:07 -0700

    iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,12 @@
-// ITERATION 1
+function updateSubtotal() {
+  const price = document.querySelector('.price span').innerHTML;
+  const quantity = document.querySelector('.quantity input').value;
+  console.log(price, quantity);
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  let subtotal = price * quantity;
+  document.querySelector('.subtotal span').innerHTML = subtotal;
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
-}
-
-// ITERATION 4
-
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
-
-// ITERATION 5
-
-function createProduct() {
-  //... your code goes here
-}
-
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
-});
+ document.querySelector('#calculate').onclick = function (e) {
+  updateSubtotal();
+} 
