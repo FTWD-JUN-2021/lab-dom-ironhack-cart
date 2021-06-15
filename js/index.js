537276a6c82db13f589a5b59360c7eefb38f4b7e
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-06-15 16:49:28 -0400

    iteration 3 complete

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
-
 function updateSubtotal(product) {
-  const price = document.querySelector('.price span').innerHTML;
-  const quantity = document.querySelector('.quantity input').value;
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
   console.log(price, quantity);
 
   let subtotal = price * quantity;
-  document.querySelector('.subtotal span').innerHTML = subtotal;
+  product.querySelector('.subtotal span').innerHTML = subtotal;
+  return subtotal
 }
 
 document.querySelector('#calculate').onclick = function (event) {
   let rows = document.querySelectorAll('.product');
 
+  let sum = 0;  
   for (let row of rows) {
-    updateSubtotal(row);
+    sum += updateSubtotal(row);
+    console.log(sum);
   }
+  document.querySelector('#total-value span').innerText = sum;
 }
